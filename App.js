dd16ad611c561e78a25a46c9ab349f2dd3f0c6d6
Netflaire <softwareengineer260@gmail.com>
2023-10-13 07:29:02 -0700

    added some changes

--- App.js
+++ App.js
@@ -1,17 +1,22 @@
 import React, { useState } from 'react' ;
-import { StyleSheet, Text, View, TouchableOpacity, Alert }from 'react-native' ;
+import { StyleSheet, Text, View, TouchableOpacity, Alert, ScrollView }from 'react-native' ;
+import { StatusBar } from 'expo-status-bar';
 	export default function App() {
 	// State variables
-		const [displayValue, setDisplayValue] = useState( '0' );
-		const [displayAns, setDisplayAns] = useState( '0' );
+		const [displayValue, setDisplayValue] = useState( '' );
+		const [displayAns, setDisplayAns] = useState( '' );
 		const [operator, setOperator] = useState( null);
 		const [firstValue, setFirstValue] = useState( '' );
 		const MAX_DISPLAY_LENGTH = 13; // Adjust the maximum length as needed
+		const FIRST_VALUE_MIN = 6;
+		const DISPLAY_VALUE_MAX = 8
+		const FIRST_VALUE_MIDDLE= 9
+
 		
 		// Function to handle number inputs
 		const handleNumberInput = (num) => {
 			if (displayValue.length < MAX_DISPLAY_LENGTH) {
-				if (displayValue === '0' ) {
+				if (displayValue === '' ) {
 					setDisplayValue(num.toString());
 				} else {
 					setDisplayValue(displayValue + num.toString());
@@ -23,48 +28,68 @@ import { StyleSheet, Text, View, TouchableOpacity, Alert }from 'react-native' ;
 
 
 		// Function to handle operator inputs
-		const handleOperatorInput = (operator) => {
+		const handleOperatorInput = (newOperator) => {
 			if (displayValue.length < MAX_DISPLAY_LENGTH) {
-				if (displayValue !== '') {
-					setOperator(operator);
-					setFirstValue(displayValue + operator);
-					setDisplayValue("0");
-			}
-			}
-		};
-
-		const calculateFontSize = () => {
-			const displayLength = displayValue.length;
-		      
-			if (displayLength <= 5) {
-			  return 100; // Set your desired font size for shorter strings
-			}
-			 else if(displayLength <= 9  && displayLength >= 5){
-			  // Calculate a font size based on the length of the string
-			  return 70; // Adjust the divisor as needed for your design
-			}
-			 else if(displayLength <= 13  && displayLength >= 9){
-			  // Calculate a font size based on the length of the string
-			  return 50; // Adjust the divisor as needed for your design
-			}else{
-				return 40
+			  if (displayValue !== '') {
+			    if (operator) {
+			      // If an operator is already set, calculate the result and update firstValue
+			      const num1 = parseFloat(firstValue);
+			      const num2 = parseFloat(displayValue);
+			      const result = calculateResult(num1, num2, operator);
+			      setFirstValue(result);
+			      setDisplayValue('');
+			    } else {
+				setFirstValue(displayValue);
+				setDisplayValue('');
+			    }
+			    setOperator(newOperator);
+			  }
 			}
 		      };
+		      
+		      // Function to calculate the result
+		      const calculateResult = (num1, num2, currentOperator) => {
+			switch (currentOperator) {
+			  case '+':
+			    return (num1 + num2).toString();
+			  case '-':
+			    return (num1 - num2).toString();
+			  case 'x':
+			    return (num1 * num2).toString();
+			  case '/':
+			    return (num1 / num2).toString();
+			  default:
+			    return '';
+			}
+		      };
+
+		// Function to calculate the font size for displayValue and firstValue
+		const calculateFontSize = (text) => {
+			const textLength = text.length;
+	
+			if (textLength > DISPLAY_VALUE_MAX && textLength <= FIRST_VALUE_MIN) {
+				return 50; // Set your desired font size for this range
+			} else if (textLength >= FIRST_VALUE_MIDDLE) {
+				return Math.min((textLength / FIRST_VALUE_MIN), 0.80) * 50; // Adjust the factor as needed
+			} else {
+				return 70; // Default font size if none of the conditions match
+			}
+		};
 		const calcSize = () => {
 			const displayLength = displayAns.length;
-		      
+			
 			if (displayLength <= 5) {
-			  return 40; // Set your desired font size for shorter strings
+				return 40; // Set your desired font size for shorter strings
 			}
-			 else if(displayLength <= 9  && displayLength >= 5){
-			  // Calculate a font size based on the length of the string
-			  return 30; // Adjust the divisor as needed for your design
+			else if(displayLength <= 9  && displayLength >= 5){
+				// Calculate a font size based on the length of the string
+				return 40; // Adjust the divisor as needed for your design
 			}
-			 else if(displayLength <= 13  && displayLength >= 9){
+			else if(displayLength <= 13  && displayLength >= 9){
 			  // Calculate a font size based on the length of the string
-			  return 20; // Adjust the divisor as needed for your design
+			  return 40; // Adjust the divisor as needed for your design
 			}else{
-				return 20
+				return 30
 			}
 		      };
 
@@ -74,7 +99,11 @@ import { StyleSheet, Text, View, TouchableOpacity, Alert }from 'react-native' ;
 			const num2 = parseFloat(displayValue);
 			
 			if (displayValue.length < MAX_DISPLAY_LENGTH) {
-				if (operator === '+' ) {
+				if(displayValue == ""){
+					return null,
+					setDisplayAns("Enter number")
+				}else{
+					if (operator === '+' ) {
 					setDisplayAns((num1 + num2).toString());
 				} else if (operator === '-' ) {
 					setDisplayAns((num1 - num2).toString());
@@ -85,6 +114,9 @@ import { StyleSheet, Text, View, TouchableOpacity, Alert }from 'react-native' ;
 				}
 				setOperator( null);
 				setFirstValue( '' );
+				setDisplayValue('')
+				}
+				
 			}
 		};
 
@@ -101,15 +133,23 @@ import { StyleSheet, Text, View, TouchableOpacity, Alert }from 'react-native' ;
 			setOperator( null);
 			setFirstValue( '' );
 		};
+		const del = () =>{
+			setDisplayValue(displayValue.slice(0,-1))
+			
+		}
 
 			return (
 			<View style={styles.container}>
 				<View style={styles.displayContainer}>
+					<Text style={[styles.displayText, {fontSize: calculateFontSize(displayValue)}]}>
+					{/* {operator ? firstValue + ' ' + operator + ' ' + displayValue : displayValue} */}
 
-					<Text style={[styles.displayText, {fontSize: calculateFontSize()}]}>
-						{displayValue}
+					{firstValue}{' '}
+						<Text style={styles.operatorStyle}>{operator}</Text>{' '}
+					{displayValue}
 					</Text>
-					<Text style={[styles.displayText, {fontSize: calcSize(), fontWeight: "normal"}]}>
+
+					<Text style={[styles.displayText, {fontSize: calcSize(), fontWeight: "normal", color:"#92ffde"}]}>
 						{displayAns}
 					</Text>
 				</View>
@@ -117,100 +157,105 @@ import { StyleSheet, Text, View, TouchableOpacity, Alert }from 'react-native' ;
 				<View style={styles.buttonContainer}>
 
 					<View style={[styles.row]}>
-						<TouchableOpacity style={styles.clearButton} onPress={handleClear}>
-							<Text style={styles.clearButtonText}>C</Text>
+						<TouchableOpacity style={[styles.button,styles.button2]} onPress={handleClear}>
+							<Text style={styles.clearButtonText}>AC</Text>
 						</TouchableOpacity>
 
-						<TouchableOpacity style={[styles.button,styles.buttonX, styles.operatorButton]} onPress={() => handleOperatorInput( 'x')}>
-							<Text style={[styles.buttonText,styles.operatorButtonText]}>×</Text>
-						</TouchableOpacity>
 
-						<TouchableOpacity style={[styles.button,styles.buttonD, styles.operatorButton]} onPress={() => handleOperatorInput( '/' )}>
+						<TouchableOpacity style={[styles.button, styles.operatorButton]} onPress={() => handleOperatorInput( '/' )}>
 							<Text style={[styles.buttonText,styles.operatorButtonText]}>÷</Text>
 						</TouchableOpacity>
 
-						<TouchableOpacity onLongPress={() => handleClear()} style={[styles.button,styles.buttondel, styles.backspaceButton, {height: 70,width: 70,}]} onPress={()=> setDisplayValue(displayValue.slice(0,-1)) }>
-							<Text style={[styles.operatorButtonText, {fontSize: 30}]}>del</Text>
+						<TouchableOpacity onLongPress={() => handleClear()} style={[styles.button,styles.button2, {width: 159, borderRadius: 19}]} onPress={()=> del() }>
+							<Text style={[styles.operatorButtonText, {fontSize: 30, color: "#fff"}]}>del</Text>
 						</TouchableOpacity>
 
 					</View>
 					<View style={styles.row}>
 
-					<TouchableOpacity style={[styles.button, styles.button7]} onPress={() => handleNumberInput(7)}>
+					<TouchableOpacity style={[styles.button]} onPress={() => handleNumberInput(7)}>
 							<Text style={styles.buttonText}>7</Text>
 						</TouchableOpacity>
 
-						<TouchableOpacity style={[styles.button, styles.button8]} onPress={() => handleNumberInput(8)}>
+						<TouchableOpacity style={[styles.button]} onPress={() => handleNumberInput(8)}>
 							<Text style={styles.buttonText}>8</Text>
 						</TouchableOpacity>
 
-						<TouchableOpacity style={[styles.button, styles.button9]} onPress={() => handleNumberInput(9)}>
+						<TouchableOpacity style={[styles.button]} onPress={() => handleNumberInput(9)}>
 							<Text style={styles.buttonText}>9</Text>
 						</TouchableOpacity>
 
-						<TouchableOpacity style={[styles.button, styles.buttonM, styles.operatorButton]} onPress={() => handleOperatorInput( '-' )}>
-							<Text style={[styles.buttonText,styles.operatorButtonText]}>−</Text>
+						<TouchableOpacity style={[styles.button,styles.operatorButton]} onPress={() => handleOperatorInput( 'x')}>
+							<Text style={[styles.buttonText,styles.operatorButtonText]}>×</Text>
 						</TouchableOpacity>
+
+					
 
 					</View>
 					
 					<View style={styles.row}>
 
-						<TouchableOpacity style={[styles.button, styles.button4]} onPress={() => handleNumberInput(4)}>
+						<TouchableOpacity style={[styles.button]} onPress={() => handleNumberInput(4)}>
 							<Text style={styles.buttonText}>4</Text>
 						</TouchableOpacity>
 
-						<TouchableOpacity style={[styles.button, styles.button5]} onPress={() => handleNumberInput(5)}>
+						<TouchableOpacity style={[styles.button]} onPress={() => handleNumberInput(5)}>
 							<Text style={styles.buttonText}>5</Text>
 						</TouchableOpacity>
 
-						<TouchableOpacity style={[styles.button, styles.button6]} onPress={() => handleNumberInput(6)}>
+						<TouchableOpacity style={[styles.button]} onPress={() => handleNumberInput(6)}>
 							<Text style={styles.buttonText}>6</Text>
 						</TouchableOpacity>
 
-						<TouchableOpacity style={[styles.button,styles.buttonP, styles.operatorButton]} onPress={() => handleOperatorInput( '+' )}>
-							<Text style={[styles.buttonText,styles.operatorButtonText]}>+</Text>
+						<TouchableOpacity style={[styles.button, styles.operatorButton]} onPress={() => handleOperatorInput( '-' )}>
+							<Text style={[styles.buttonText,styles.operatorButtonText]}>−</Text>
 						</TouchableOpacity>
+
+						
 
 
 						
 
 					</View>
 
-					<View style={[styles.row, {justifyContent: "space-between"}]}>
-						<View>
-							<View style={[{flexDirection: "row", }]}>
-									<TouchableOpacity style={[styles.button, styles.button1]} onPress={() => handleNumberInput(1)}>
+					<View style={[styles.row]}>
+									<TouchableOpacity style={[styles.button]} onPress={() => handleNumberInput(1)}>
 										<Text style={styles.buttonText}>1</Text>
 									</TouchableOpacity>
 
-									<TouchableOpacity style={[styles.button, styles.button2]} onPress={() => handleNumberInput(2)}>
+									<TouchableOpacity style={[styles.button]} onPress={() => handleNumberInput(2)}>
 										<Text style={styles.buttonText}>2</Text>
 									</TouchableOpacity>
 
-									<TouchableOpacity style={[styles.button, styles.button3]} onPress={() => handleNumberInput(3)}>
+									<TouchableOpacity style={[styles.button]} onPress={() => handleNumberInput(3)}>
 										<Text style={styles.buttonText}>3</Text>
 									</TouchableOpacity>
-							</View>
 
-								<TouchableOpacity style={[styles.button, styles.zeroButton,styles.button0]} onPress={() => handleNumberInput(0)}>
-									<Text style={[styles.buttonText,styles.zeroButtonText]}>0</Text>
-								</TouchableOpacity>
+									<TouchableOpacity style={[styles.button, styles.operatorButton]} onPress={() => handleOperatorInput( '+' )}>
+										<Text style={[styles.buttonText,styles.operatorButtonText]}>+</Text>
+									</TouchableOpacity>
 
-										
-								<TouchableOpacity style={[styles.button, styles.buttonPoint]} onPress={handleDecimalPoint}>
-									<Text style={[styles.operatorButtonText, {fontSize: 30}]}>.</Text>
-								</TouchableOpacity>
-
-						</View>
-
-						<TouchableOpacity style={[styles.equalButton, styles.buttonE]} onPress={handleEqual} >
-							<Text style={styles.equalButtonText}>=</Text>
-						</TouchableOpacity>
 
 					</View>
+
+					<View style={[styles.row]}>
+							<TouchableOpacity style={[styles.button, styles.zeroButton, {width: 159, borderRadius: 19}]} onPress={() => handleNumberInput(0)}>
+								<Text style={[styles.buttonText,styles.zeroButtonText]}>0</Text>
+							</TouchableOpacity>
+
+									
+							<TouchableOpacity style={[styles.button]} onPress={handleDecimalPoint}>
+								<Text style={[styles.operatorButtonText, {fontSize: 30}]}>.</Text>
+							</TouchableOpacity>
+
+
+							<TouchableOpacity style={[styles.button, styles.operatorButton]} onPress={handleEqual} >
+								<Text style={styles.equalButtonText}>=</Text>
+							</TouchableOpacity>
+						</View>
 					
 			</View>
+			<StatusBar backgroundColor='#0e0e0e'/>
 		</View>
 		);
 }
@@ -227,10 +272,9 @@ import { StyleSheet, Text, View, TouchableOpacity, Alert }from 'react-native' ;
 			justifyContent: "flex-end" ,
 			alignItems: "flex-end",
 			padding: 20,
-			width: "100%",
-			backgroundColor: "#1b1b1b",
-			borderBottomLeftRadius: 27,
-			borderBottomRightRadius: 27
+			margin: 10,
+			width: 400 ,
+			backgroundColor: "#0e0e0e",
 		},
 		displayText: {
 			// fontSize: 90,
@@ -241,115 +285,28 @@ import { StyleSheet, Text, View, TouchableOpacity, Alert }from 'react-native' ;
 		buttonContainer: {
 			flex: 3,
 			width: '93%' ,
-			marginBottom: 20
+			marginBottom: -30
 		},
 		row: {
-			flex: 1,
 			flexDirection: 'row' ,
 			justifyContent: 'space-between' ,
 			marginBottom: 10,
 		},
 		button: {
-			borderRadius: 15,
+			borderRadius: 50,
 			alignItems: 'center' ,
-			justifyContent: 'space-between' ,
+			// justifyContent: 'space-between' ,
 			backgroundColor: '#4e4e4eab' ,
-			elevation: 3,
-			margin: 2,
+			margin: 4,
 			height:70,
 			width:70,
 			padding: 15,
 		},
-		buttonX: {
-			position: "absolute",
-			left: 85,
-			bottom:21
-		},
-		buttonD: {
-			position: "absolute",
-			right: 84,
-			bottom:21
-		},
-		buttondel: {
-			position: "absolute",
-			right: -3,
-			bottom:21
-		},
-		button7: {
-			position: "absolute",
-			left: -3,
-			bottom:39
-		},
-		button8: {
-			position: "absolute",
-			left: 85,
-			bottom:39
-		},
-		button9: {
-			position: "absolute",
-			right: 84,
-			bottom:39
-		},
-		buttonM: {
-			position: "absolute",
-			right: -3,
-			bottom:39,
-			backgroundColor: "#ffae00"
-		},
-		button4: {
-			position: "absolute",
-			left: -3,
-			bottom:57
-		},
-		button5: {
-			position: "absolute",
-			left: 85,
-			bottom:57
-		},
-		button6: {
-			position: "absolute",
-			right: 84,
-			bottom:57
-		},
-		buttonP: {
-			position: "absolute",
-			right: -3,
-			bottom:57
-		},
-		button1: {
-			position: "absolute",
-			left: -3,
-			bottom:-20
-		},
 		button2: {
-			position: "absolute",
-			left: 85,
-			bottom:-20
+			backgroundColor: "#25ecb1",
+			opacity: 0.9
 		},
-		button3: {
-			position: "absolute",
-			left: 180,
-			bottom:-20
-		},
-		buttonPoint: {
-			position: "absolute",
-			left: 180,
-			bottom: -4
-		},
-		button0: {
-			position: "absolute",
-			left: -3,
-			bottom: -4
-		},
-		buttonE: {
-			position: "absolute",
-			width: 70,
-			height:150,
-			right: -3,
-			bottom: -4,
-			borderRadius: 15
-		},
-		buttonText: {
+			buttonText: {
 			fontSize: 34,
 			color: '#ffffff' ,
 		},
@@ -359,18 +316,18 @@ import { StyleSheet, Text, View, TouchableOpacity, Alert }from 'react-native' ;
 			// paddingRight: 35,
 			textAlign: "center",
 			justifyContent: "center",
-			width: 160
+			width: 70
 		},
 		zeroButtonText: {
 			// marginLeft: 10,
 			fontSize: 35,
 		},
 		operatorButton: {
-			backgroundColor: '#ffae00' ,
+			backgroundColor: '#e9e6e1' ,
 
 		},
 		operatorButtonText: {
-			color: '#ffffff' ,
+			color: '#09a84c' ,
 		},
 		equalButton: {
 			flex: 1,
@@ -385,23 +342,14 @@ import { StyleSheet, Text, View, TouchableOpacity, Alert }from 'react-native' ;
 		},
 		equalButtonText: {
 			fontSize: 32,
-			color: '#fff' ,
-		},
-		clearButton: {
-			borderRadius: 50,
-			alignItems: 'center' ,
-			justifyContent: 'center' ,
-			opacity: 0.8,
-			backgroundColor: '#91051c' ,
-			height: 70,
-			width: 70,
-			marginTop: 10,
-			elevation: 3,
-			padding: 10,
+			color: '#09a84c' ,
 		},
 		clearButtonText: {
 			fontSize: 24,
-			color: '#e27688' ,
+			color: '#fff' ,
 		},
+		operatorStyle: {
+			color: "#92ffde"
+		}
 	});
 
